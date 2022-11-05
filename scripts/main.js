f85bfccb339cd18ba7696c1ea174f293fc242539
Talddec <85902255+Talddec@users.noreply.github.com>
2022-11-05 20:19:23 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Events.on(ClientLoadEvent, b  => {
-Units.alpha.range=500000
-Units.alpha.damage=500000
-Units.alpha.speed=500000
+Units.alpha.range=500
+Units.alpha.damage=500
+Units.alpha.speed=500
 });
